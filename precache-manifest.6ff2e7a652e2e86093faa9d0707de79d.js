fff3ef8291f579ddedcc15f11efa6a01e378beca
Karina <karina_karapetyan@mail.ru>
2022-10-03 22:45:13 +0300

    Updates

---
+++ precache-manifest.6ff2e7a652e2e86093faa9d0707de79d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b49affdffe112009531c6bab4b95654a",
+    "url": "/todo-app-react/index.html"
+  },
+  {
+    "revision": "ccf3a85e0c5e9fbce92f",
+    "url": "/todo-app-react/static/css/main.8e2b3bc0.chunk.css"
+  },
+  {
+    "revision": "feb1b647066370ea1297",
+    "url": "/todo-app-react/static/js/2.df0683b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app-react/static/js/2.df0683b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccf3a85e0c5e9fbce92f",
+    "url": "/todo-app-react/static/js/main.4eb6603c.chunk.js"
+  },
+  {
+    "revision": "77f6b8944eda1092f996",
+    "url": "/todo-app-react/static/js/runtime-main.a4662593.js"
+  }
+]);
